24aa69b9ba8fc77f81571ff8fc9292d2bdb4a6e9
YuChen-Qian <clydeqian@gmail.com>
2015-12-23 10:09:46 +0800

    add basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
-  get 'blog'   =>  'static_pages#blog'
-
   root          'static_pages#home'
 
 # create an individual article without comments
+# add blog features to "articles"
 end
